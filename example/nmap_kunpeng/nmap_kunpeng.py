a4783c38ec4c669362b77928eba98d996d743142
wolf <wolf_cn@outlook.com>
2019-01-17 21:49:07 +0800

    nmap例子调整

--- example/nmap_kunpeng/nmap_kunpeng.py
+++ example/nmap_kunpeng/nmap_kunpeng.py
@@ -14,13 +14,12 @@ so.SetConfig.argtypes = [c_char_p]
 
 nm = nmap.PortScanner()
 def scan(hostname,ports):
+    result = []
     nm.scan(hostname,ports,'--open')
     task_list = []
-    # print(nm.get_nmap_last_output())
     for host in nm.all_hosts():
         if nm[host]['hostnames']:
             domain = nm[host]['hostnames'][0]['name']
-            print(domain)
         for proto in nm[host].all_protocols():
             lport = nm[host][proto].keys()
             for port in lport:
@@ -32,17 +31,21 @@ def scan(hostname,ports):
                     if name in ['https','ssl'] or port == 443:
                         scheme = 'https'
                     ip_url = '{}://{}:{}'.format(scheme,host,port)
-                    domain_url = '{}://{}:{}'.format(scheme,domain,port)
                     task_list.append({'type':'web','target': 'web', 'netloc': ip_url})
-                    task_list.append({'type':'web','target': 'web', 'netloc': domain_url})
+                    if domain != '' and domain != host:
+                        domain_url = '{}://{}:{}'.format(scheme,domain,port)
+                        task_list.append({'type':'web','target': 'web', 'netloc': domain_url})
                     if product == '' or product == name: continue
                     task_list.append({'type':'web','target': product, 'netloc': ip_url})
-                    task_list.append({'type':'web','target': product, 'netloc': domain_url})
+                    if domain != '' and domain != host:
+                        task_list.append({'type':'web','target': product, 'netloc': domain_url})
                 else:
                     task_list.append({'type':'service','target': name, 'netloc': host + ':' + str(port)})
     for task in task_list:
         out = so.Check(json.dumps(task))
-        print(json.loads(out))
+        result+= json.loads(out)
+    return result
 
 if __name__ == '__main__':
-    scan('www.baidu.com', '80,443')
+    result = scan('www.baidu.com', '80,443')
+    print(result)
