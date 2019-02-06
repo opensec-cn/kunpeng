bf356c6fcf5b0cbe1446b8aae0ccc403b2b73546
wolf <wolf_cn@outlook.com>
2019-02-06 15:00:31 +0800

    辅助验证 增加默认2秒延时，防止漏报

--- util/aider.go
+++ util/aider.go
@@ -3,11 +3,13 @@ package util
 import (
 	"net/http"
 	"strings"
+	"time"
 	. "github.com/opensec-cn/kunpeng/config"
 )
 
 // AiderCheck 辅助验证，使用标识字符串判断漏洞是否存在（触发漏洞会把标识字符串传输到辅助脚本上，如果查询存在，说明存在漏洞）
 func AiderCheck(randStr string) bool {
+	time.Sleep(time.Duration(2) * time.Second)
 	request, _ := http.NewRequest("GET", Config.Aider+"/check/"+randStr, nil)
 	resp, err := RequestDo(request, true)
 	if err != nil {
