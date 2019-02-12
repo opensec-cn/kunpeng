e9b3ba33bb713a26320a94dd9e7071512fd29e5e
wolf <wolf_cn@outlook.com>
2019-02-13 00:47:52 +0800

    增加 http代理未授权访问验证插件

---
+++ plugin/go/httpProxyUnauth.go
@@ -0,0 +1,70 @@
+package goplugin
+
+import (
+	"fmt"
+	"strings"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"crypto/tls"
+	"time"
+	
+	"github.com/opensec-cn/kunpeng/plugin"
+	. "github.com/opensec-cn/kunpeng/config"
+)
+
+type httpProxyUnauth struct {
+	info   plugin.Plugin
+	result []plugin.Plugin
+}
+
+func init() {
+	plugin.Regist("proxy", &httpProxyUnauth{})
+}
+func (d *httpProxyUnauth) Init() plugin.Plugin {
+	d.info = plugin.Plugin{
+		Name:    "HTTP代理 未授权访问",
+		Remarks: "攻击者可使用此代理作为跳板访问内部网络资源。",
+		Level:   1,
+		Type:    "UNAUTH",
+		Author:  "wolf",
+	}
+	return d.info
+}
+func (d *httpProxyUnauth) GetResult() []plugin.Plugin {
+	return d.result
+}
+func (d *httpProxyUnauth) Check(netloc string, meta plugin.TaskMeta) bool {
+	var proxyURL string
+	if strings.IndexAny(netloc, "http") == 0 {
+		proxyURL = netloc
+	}else{
+		proxyURL = "http://" + netloc
+	}
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse(proxyURL)
+	}
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy:           proxy,
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Timeout:   time.Second * time.Duration(Config.Timeout),
+	}
+	resp, err := client.Get("https://www.apple.com/contact/")
+	if err != nil {
+		return false
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	if strings.Contains(string(body),"Contacting Apple"){
+		result := d.info
+		result.Request = proxyURL
+		result.Remarks = fmt.Sprintf("HTTP代理无身份认证，%s", result.Remarks)
+		d.result = append(d.result, result)
+		return true
+	}
+	return false
+}
