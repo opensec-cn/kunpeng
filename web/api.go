cd8f9dc4ed93db0eb5e9b242be96a286767d08b9
v1xingyue <qixingyue@126.com>
2019-02-26 09:19:02 +0800

    用datatable做一个plugin的展示界面，插件数目不大的时候，用这个界面搜索足够。使用方法: StartWebServer 以后 ， 访问 /pluginList

--- web/api.go
+++ web/api.go
@@ -31,5 +31,59 @@ func StartServer(bindAddr string) {
 		c.JSON(200, map[string]bool{"success": true})
 	})
 
+    router.GET("/pluginList",func(c *gin.Context) {
+        c.Header("Content-type","text/html;charset=utf-8")
+        c.String(200,`
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>北冥有鱼，其名为鲲</title>
+        <meta name="description" content="" />
+    </head>
+    <body>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.js"></script>
+        <link href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" rel="stylesheet" />
+        <link href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" rel="stylesheet" />
+        <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script src="//cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
+        <div class="panel panel-default"> 
+            <div class="panel-heading">鲲之大，不知其几千里也! 如果列表为空，可能cdn资源没有加载到，请检查网络</div> 
+            <div class="panel-body">
+                <table id="pluginListTable" class="table table-striped table-bordered" > 
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>author</th>
+                                <th>remarks</th>
+                                <th>target</th>
+                            </tr>
+                        </thead>
+                </table>
+            </div>
+        </div>
+        <script>
+            $(function(){
+                $("#pluginListTable").DataTable({
+                    "pageLength":25,
+                    "ajax":{
+                        "url":"/api/pluginList",
+                        "dataSrc":""
+                    },
+                    "columns":[
+                        {"data":"name"},
+                        {"data":"author"},
+                        {"data":"remarks"},
+                        {"data":"target"}
+                    ]
+                });
+            });
+        </script>
+    </body>
+</html>
+        `) 
+    })
+
 	router.Run(bindAddr)
 }
