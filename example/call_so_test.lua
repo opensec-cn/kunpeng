b73a83723f54f5b06eb0b965f2410dbe8caf22e1
v1xingyue <qixingyue@126.com>
2019-03-07 21:01:53 +0800

    修改导出函数命名

--- example/call_so_test.lua
+++ example/call_so_test.lua
@@ -12,10 +12,11 @@ ffi.cdef[[
     char* GetPlugins();
     void SetConfig(char* p0);
     void ShowLog();
+    void StartBuffer();
+    char* GetLog(char* sep);
 ]]
 
-
-kunpeng.ShowLog()
+--- kunpeng.ShowLog()
 --- 用ffi.string 格式化char* 返回的结果，直到\0 结束字符串
 local plugins_str  = ffi.string(kunpeng.GetPlugins())
 print(plugins_str)
@@ -35,19 +36,18 @@ local result_str = ffi.string(kunpeng.Check(taskArg))
 print(result_str)
 
 
---- 测试logBuffer 的内容返回
+--- 测试logBuffer 的返回
+print("============  logBuffer Start Testing.... ==========")
 kunpeng.StartBuffer()
 local result_str = ffi.string(kunpeng.Check(taskArg))
-local scan_log = ffi.string(kunpeng.BufferContent(ffi.cast('char *',"")))
+local scan_log = ffi.string(kunpeng.GetLog(ffi.cast('char *',"")))
 print("result:")
 print(result_str)
 print("scan_log:")
 print(scan_log)
 
-
-
+print("============  logBuffer Start End .... ==========")
 --- 测试StartWebServer 
 kunpeng.StartWebServer( ffi.cast('char *',"0.0.0.0:3000") )
 io.stdin:read("*line")
-
 
